4cf45b7ac709eafee2d2af13a69450b793b2e5d1
Katia <katia.zakharina@gmail.com>
2022-05-18 00:14:08 +0300

    feat: add noMatch page

--- src/pages/NoMatch/NoMatch.tsx
+++ src/pages/NoMatch/NoMatch.tsx
@@ -1,5 +1,18 @@
-import { StyledNoMatch } from './StyledNoMatch';
+import { useTranslation } from 'react-i18next';
+import { ThemedSVG } from 'components/ThemedSVG/ThemedSVG';
+import { Message, StyledNoMatch, Title } from './StyledNoMatch';
+
+import warningIcon from 'assets/svg/warning.svg';
 
 export function NoMatch() {
-  return <StyledNoMatch>404</StyledNoMatch>;
+  const { t } = useTranslation();
+  return (
+    <StyledNoMatch>
+      <div>
+        <ThemedSVG src={warningIcon} width="2rem" height="2rem" />
+        <Title>{t('This page is not found')}</Title>
+        <Message to="/">{t('Go to the main page')}</Message>
+      </div>
+    </StyledNoMatch>
+  );
 }
--- src/pages/NoMatch/StyledNoMatch.ts
+++ src/pages/NoMatch/StyledNoMatch.ts
@@ -1,3 +1,21 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
-export const StyledNoMatch = styled.div``;
+import { ColumnContainer } from 'components/Layout';
+
+export const StyledNoMatch = styled(ColumnContainer)`
+  justify-content: center;
+`;
+
+export const Title = styled.h1`
+  display: inline-block;
+  margin-left: 10px;
+  font-size: 2rem;
+`;
+export const Message = styled(Link)`
+  display: block;
+  margin-top: 20px;
+  font-size: 1.4rem;
+  color: ${({ theme }) => theme.colors.inverse_main};
+  text-align: center;
+`;
